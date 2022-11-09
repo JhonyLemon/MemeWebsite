5fd176d41afb331a8cc13ab1b12db70f0881f646
Adam N <76851495+JhonyLemon@users.noreply.github.com>
2022-11-09 19:59:19 +0100

    Initial backend commit

---
+++ meme_backend/src/main/java/pl/jhonylemon/meme_backend/MemeBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.jhonylemon.meme_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemeBackendApplication.class, args);
+    }
+
+}
